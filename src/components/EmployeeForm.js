8ccaaf886dc71f95ed9aed527e61d24fe7567448
mattsuzuki <matthewjamessuzuki@gmail.com>
2023-05-17 19:52:00 -0700

    push progress

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,11 +1,24 @@
 // EmployeeForm.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import './EmployeeForm.css';
 
 export default function EmployeeForm() {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [credential, setCredential] = useState('');
+    const [days, setDays] = useState([]);
+    const [shifts, setShifts] = useState([]);
+
+    const handleDayChange = (e) => {
+        const selectedDays = Array.from(e.target.selectedOptions, (option) => option.value);
+        setDays(selectedDays);
+    };
+
+    const handleShiftChange = (e) => {
+        const selectedShifts = Array.from(e.target.selectedOptions, (option) => option.value);
+        setShifts(selectedShifts);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -13,7 +26,9 @@ export default function EmployeeForm() {
             const employeeData = {
                 name,
                 phoneNumber,
-                credential
+                credential,
+                days,
+                shifts,
             };
             const response = await axios.post('http://localhost:3001/employees', employeeData);
             console.log(response.data);
@@ -35,6 +50,26 @@ export default function EmployeeForm() {
             <label>
                 Credential:
                 <input type="text" value={credential} onChange={(e) => setCredential(e.target.value)} />
+            </label>
+            <label>
+                Days:
+                <select multiple value={days} onChange={handleDayChange}>
+                    <option value="Monday">Monday</option>
+                    <option value="Tuesday">Tuesday</option>
+                    <option value="Wednesday">Wednesday</option>
+                    <option value="Thursday">Thursday</option>
+                    <option value="Friday">Friday</option>
+                    <option value="Saturday">Saturday</option>
+                    <option value="Sunday">Sunday</option>
+                </select>
+            </label>
+            <label>
+                Shifts:
+                <select multiple value={shifts} onChange={handleShiftChange}>
+                    <option value="Morning">Morning</option>
+                    <option value="Afternoon">Afternoon</option>
+                    <option value="NOC">NOC</option>
+                </select>
             </label>
             <button type="submit">Add Employee</button>
         </form>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,33 @@ import { Link } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
 
 function HomePage() {
+    const [searchCredential, setSearchCredential] = React.useState('');
+    const [selectedDate, setSelectedDate] = React.useState('');
+
+    const handleSearchChange = (e) => {
+        setSearchCredential(e.target.value);
+    };
+
+    const handleDateChange = (e) => {
+        setSelectedDate(e.target.value);
+    };
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        // Perform search logic based on searchCredential and selectedDate values
+        // Redirect or perform any necessary actions
+        if (searchCredential !== '') {
+            // Redirect to employee search page passing the searchCredential and selectedDate as URL parameters
+            window.location.href = `/employee-search?credential=${encodeURIComponent(searchCredential)}&date=${encodeURIComponent(selectedDate)}`;
+        }
+    };
+
     return (
         <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh" bgcolor="#f5f5f5">
             <Typography variant="h2" gutterBottom>Staffing Coordinator Portal</Typography>
@@ -13,7 +38,32 @@ function HomePage() {
                 <Button variant="contained" color="primary" component={Link} to="/employee-list" style={{ marginRight: '20px' }}>View Employees</Button>
                 <Button variant="contained" color="primary" component={Link} to="/employees/new" style={{ marginRight: '20px' }}>Add Employee</Button>
                 <Button variant="contained" color="primary" component={Link} to="/shift-list">View Shifts</Button>
-
+            </Box>
+            <Box mt={5}>
+                <form onSubmit={handleSearchSubmit}>
+                    <FormControl>
+                        <Select value={searchCredential} onChange={handleSearchChange} displayEmpty style={{ marginRight: '1em' }}>
+                            <MenuItem value="" disabled>
+                                Search Employees by Credential
+                            </MenuItem>
+                            <MenuItem value="RN">RN</MenuItem>
+                            <MenuItem value="LVN/PT">LVN/PT</MenuItem>
+                            <MenuItem value="BHS">BHS</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <TextField
+                        id="selectedDate"
+                        label="Select Date"
+                        type="date"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        style={{ marginRight: '1em' }}
+                    />
+                    <Button type="submit" variant="contained" color="primary">Search</Button>
+                </form>
             </Box>
         </Box>
     );
